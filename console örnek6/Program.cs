b3e4dd218e191dfc78a1c6b027f88fca24e192a7
Emirhan Çalışkan <emrhncalskn@gmail.com>
2022-02-16 22:05:09 +0300

    Verilen sayıya kadar olan asal sayıları bulma

---
+++ console örnek6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Bir sayı giriniz: ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+int sayac2 = 0;
+
+for (int y = 2; y <= sayi2; y++)
+{
+    sayac2 = 0;
+
+    for (int x = 2; x < y ; x++)
+    {
+        if (y % x == 0)
+        {
+            sayac2++;
+            break;
+        }
+    }
+    if (sayac2 == 0)
+    {
+        Console.Write(y + "-");
+    }
+
+}
+
+//Verilen sayıya kadar olan asal sayıları bulma
